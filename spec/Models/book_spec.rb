dda4a08a27036b012dd1ed099f5ad627555dcca3
Christopher Lynch <YOUR-EMAIL-ADDRESS>
2015-01-22 15:43:56 -0800

    completed two tests

--- spec/Models/book_spec.rb
+++ spec/Models/book_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Book, :type => :model do
- it "should have a valid title title"
- it "should have an author"
+ it "should have a valid title title" do 
+ 	expect(FactoryGirl.build(:book)).to be_valid
+ end
+
+
+
+ it "should have an author" do
+ 	expect(FactoryGirl.build(:book)).to be_valid
+ end
+
  it "should have a page_count"
 end
